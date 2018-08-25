62074f03e7a21f4bb17bdad1e6da99f73acace98
Clyde Migue <clyde.migue@gmail.com>
2018-08-25 16:40:00 +0800

    added Google Auth(Sign-In)

--- app/src/main/java/ph/com/agrinotes/agrinotes/MainActivity.java
+++ app/src/main/java/ph/com/agrinotes/agrinotes/MainActivity.java
@@ -1,8 +1,12 @@
 package ph.com.agrinotes.agrinotes;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,12 +17,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int RC_SIGN_IN = 123 ;
+    private static final String TAG = "MainActivity";
     private FirebaseAuth auth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +57,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
+        // initialize firebase auth
+        auth = FirebaseAuth.getInstance();
         // get the user
-        FirebaseUser currentUser = auth.getCurrentUser();
+        currentUser = auth.getCurrentUser();
+        if( currentUser == null)
+        {
+            Log.d(TAG, "No current user");
+            createSignInIntent();
+        }
     }
 
     @Override
@@ -58,7 +76,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+        if(currentUser == null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle("WARNING");
+            builder.setMessage("You need to sign-in in order to use the app");
+            builder.setPositiveButton("Sign-In", dialogListener);
+            builder.setNegativeButton("Exit", dialogListener);
+            builder.show();
+        }
     }
+
+    private DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    createSignInIntent();
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    finish();
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -105,5 +147,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void createSignInIntent(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "Successful Login");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                Log.d(TAG, "Unsuccessful Login");
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setCancelable(true);
+                builder.setTitle("WARNING");
+                builder.setMessage("You need to sign-in in order to use the app");
+                builder.setPositiveButton("Sign-In", dialogListener);
+                builder.setNegativeButton("Exit", dialogListener);
+                builder.show();
+            }
+        }
     }
 }
