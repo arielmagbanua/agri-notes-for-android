b048b17561627442c000b6301f655ba0ff628a61
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-08-25 14:56:47 +0800

    Added basic fragments and fragments switching.

--- app/src/main/java/ph/com/agrinotes/agrinotes/MainActivity.java
+++ app/src/main/java/ph/com/agrinotes/agrinotes/MainActivity.java
@@ -1,8 +1,11 @@
 package ph.com.agrinotes.agrinotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,13 +15,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth auth;
+    private LocationCallback locationCallback;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private FragmentManager fragmentManager;
+
+    // Fragments
+    private NotesFragment notesFragment = new NotesFragment();
+    private MapFragment mapFragment = new MapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +41,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -46,8 +51,60 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // initialize firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
         // get the user
         FirebaseUser currentUser = auth.getCurrentUser();
+
+        //set initially the findYourJeepFragment
+        fragmentManager = getSupportFragmentManager();
+
+        //set the first item in navigation view
+        MenuItem firstItem = navigationView.getMenu().getItem(0);
+        setCurrentFragment(firstItem.getItemId());
+        firstItem.setChecked(true);
+        setTitle(firstItem.getTitle());
+    }
+
+    private void setCurrentFragment(int itemId) {
+        Fragment fragment = null;
+        Intent intent = null;
+        int requestCode = 0;
+
+        switch (itemId){
+            case R.id.notes:
+
+                fragment = notesFragment;
+
+                break;
+
+            case R.id.map:
+
+                fragment = mapFragment;
+
+                break;
+
+            default:
+                Toast.makeText(this, "Unknown Item!", Toast.LENGTH_SHORT).show();
+        }
+
+        if(fragment != null){
+            if(!isFragmentActive(fragment)){
+                Bundle args = new Bundle();
+                args.putInt("item_id", itemId);
+                fragment.setArguments(args);
+
+                // Insert the fragment by replacing any existing fragment
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, fragment)
+                        .commit();
+            }
+        }
+    }
+
+    public boolean isFragmentActive(Fragment fragment) {
+        return fragment.isAdded() && !fragment.isDetached() && !fragment.isRemoving();
     }
 
     @Override
@@ -88,19 +145,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        setCurrentFragment(id);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/ph/com/agrinotes/agrinotes/MapFragment.java
@@ -0,0 +1,25 @@
+package ph.com.agrinotes.agrinotes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.maps_fragment_layout, container, false);
+        return rootView;
+    }
+}
